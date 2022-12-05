8a2d94f110790d367a0086cc156a8a03715a20a2
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-12-05 16:18:47 -0500

    feat; add error boundaries class

---
+++ src/utilities/ErrorBoundaries.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface Props {
+  children: React.ReactNode
+  fallBackComponent: React.ReactNode
+
+}
+
+interface State {
+  hasError: boolean
+}
+
+export class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true }
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallBackComponent
+    }
+
+    return this.props.children
+  }
+}
