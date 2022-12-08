0b602890e2a9ae863c82d77b266882655c99e776
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-12-08 07:13:25 -0500

    feat: component test with count and button

--- src/components/ComponentTest.tsx
+++ src/components/ComponentTest.tsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+
 function ComponentTest({ name }: { name: string }) {
-  return <div>{name.toUpperCase()}</div>
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      {name.toUpperCase()}
+      {count}
+      <button onClick={handleClick}>Click me!</button>
+    </div>
+  );
 }
 
-export default ComponentTest
+export default ComponentTest;
