38ae15e3857d92f97ee242c9896026204efa26f5
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-12-08 07:20:32 -0500

    feat: fetch with api

---
+++ src/components/ComonentTestAsync.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { ErrorBoundary } from '../utilities';
+
+function componentTestAsync() {
+  const [result, setResult] = useState();
+  const [error, setError] = useState<boolean>(false);
+
+  let url = 'https://rickandmortyapi.com/api/characters/1";';
+  const fetchRickAndMorty = async () => {
+    const response = await fetch(url)
+      .then(r => {
+        if (r.status !== 200) throw new Error('Error');
+        setError(false);
+        return r;
+      })
+      .catch(error => {
+        setError(true);
+        url = 'https://rickandmortyapi.com/api/character/1';
+        throw new Error(error);
+      });
+
+      const data = await response.json()
+      setResult(data)
+  };
+}
+
+export default componentTestAsync;
