c6a23918b0c31a839e9265d5d0cfd4aafce80790
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-12-10 08:02:11 -0500

    feat: add methods to error boundaries

--- src/utilities/ErrorBoundaries.tsx
+++ src/utilities/ErrorBoundaries.tsx
@@ -3,20 +3,33 @@ import React from 'react'
 interface Props {
   children: React.ReactNode
   fallBackComponent: React.ReactNode
-
+  resetCondition?: any
+  error?: boolean
 }
 
 interface State {
   hasError: boolean
+  resetCondition: any
 }
 
 export class ErrorBoundary extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props)
-    this.state = { hasError: false }
+    this.state = { hasError: false, resetCondition: props.resetCondition }
   }
 
   static getDerivedStateFromError(error: Error) {
+    return { hasError: true }
+  }
+
+  static getDervedStateFromProps(props: Props, state: State) {
+    if (props.resetCondition !== state.resetCondition) {
+      return { hasError: false, resetCondition: props.resetCondition }
+    }
+    return null
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
     return { hasError: true }
   }
 
